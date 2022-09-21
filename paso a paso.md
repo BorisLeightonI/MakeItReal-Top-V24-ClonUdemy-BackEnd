36bcf87efc58a55f183637bd146b46b8dc32d08f
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2022-09-21 10:11:14 -0500

    feat database connection working

--- paso a paso.md
+++ paso a paso.md
@@ -13,3 +13,9 @@
 ## first CRUD
 
 para id randoms ```npm i uuid-random```
+
+## arquitectura
+
+instalar mongoose ```npm i mongoose```
+
+instalar dotenv para variables de entorno ```npm i dotenv  -D```
--- src/server.js
+++ src/server.js
@@ -1,86 +1,13 @@
-const express = require('express');
-const uuid = require("uuid-random")
+require("dotenv").config()
+const express = require("express");
+const { connect } = require("./db")
 
 const app = express();
-const port = 8080;
-app.use(express.json())// mas adelante
+const port = process.env.PORT || 8080;
+connect();
 
-//Crud create/post read/get update/put delete/delete
-//base api = http://localhost:
+app.use(express.json())
 
-//get /tasks
-//get /taskes/:taskid
-//post /tasks
-//put /tasks/:taskid
-
-let toDo = [];
-
-// get all
-app.get("/tasks", (req, res) => {
-  res.status(200).json(toDo)
-})
-
-//GET:id
-app.get("/tasks/:taskId", (req, res) => {
-  const { taskId } = req.params
-
-  const task = toDo.filter((item) => taskId === item.id)
-
-  if (task === undefined) {
-    res.status(204).json({ message: "Task not found" })
-  }
-
-  res.status(200).json(task)
-})
-
-// post
-app.post("/tasks", (req, res) => {
-  const { title } = req.body
-
-  const newTask = {
-    id: uuid(),
-    title,
-    done: false
-  }
-
-  toDo.push(newTask);
-
-  res.status(201).json(newTask)
-})
-
-
-
-//put
-app.put("/tasks/:taskId", (req, res) => {
-  const { taskId } = req.params
-
-  let updateTask;
-  toDo = toDo.map((item) => {
-    if (item.id !== taskId) {
-      return item
-    }
-
-    updateTask = {
-      ...item,
-      done: !item.done
-    }
-
-    return updateTask;
-  })
-
-  res.status(200).json(updateTask)
-})
-
-//delete
-app.delete("/tasks/:taskId", (req, res) => {
-  const { taskId } = req.params
-
-  toDo = toDo.filter((item) => item.id !== taskId)
-
-  res.status(200).json({ message: "OK" })
-})
-
-
-app.listen(port,() =>{
-  console.log(`Succesfully running at http://localhost:${port} from src/server.js`)
+app.listen(port, () => {
+  console.log(`✅Server listening on port ${port} from server.js`)
 })
