b3b95224c95d7aaa6b0e8353bc3e9c7f1573e5db
BorisLeightonI <borisleighton@gmail.com>
2022-10-07 16:51:11 -0500

    added multimedia service

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const studentRoute = require('./api/student/student.routes')
 const teacherRoute = require('./api/teacher/teacher.routes')
 const courseRoute = require('./api/course/course.routes')
 const classRoute = require('./api/class/class.routes')
+const mediaRoute = require('./api/multimedia/media.routes');
 
 function routes(app) {
   app.use("/user", userRoute);
@@ -11,7 +12,7 @@ function routes(app) {
   app.use('/teacher', teacherRoute);
   app.use('/course', courseRoute);
   app.use('/class', classRoute);
-
+  app.use(mediaRoute);
 }
 
  module.exports = routes;
