1ffdb63dcadc4e2482453c36e9a808ee1bbedd29
Andres Berrio (Andrew) <97372501+andrewsolutions81@users.noreply.github.com>
2022-10-10 08:59:53 -0500

    User/model (#10)
    * USER MODEL IS NOW THE ONLY MODEL
    
    * user created on database

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,12 @@ const courseRoute = require('./api/course/course.routes')
 const classRoute = require('./api/class/class.routes')
 
 function routes(app) {
-  app.use("/user", userRoute);
-  app.use('/student', studentRoute); 
+  app.use('/user', userRoute);
+  app.use('/student', studentRoute);
   app.use('/teacher', teacherRoute);
   app.use('/course', courseRoute);
   app.use('/class', classRoute);
 
-}
+};
 
  module.exports = routes;
