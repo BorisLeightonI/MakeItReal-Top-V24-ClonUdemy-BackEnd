7b5dcaff96049f574ab54953f5ee135aa385ced7
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2022-10-18 18:25:04 -0500

    course creation

--- src/api/course/course.model.js
+++ src/api/course/course.model.js
@@ -1,65 +1,89 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-function getPrice(num){ return (num/100).toFixed(2);}
-
-function setPrice(num){ return num*100;}
-
-const courseSchema = new mongoose.Schema({
-  active:{
-    type: Boolean,
-    default: true,
-  },
-  title : {
-    type: String,
-    required: true,
-  },
-  subtitle: {
-    type: String,
-  },
-  description: {
-    type: String,
-    // required: true,
-  },
-  language: {
-    type: String,
-    default: 'english'
-  },
-  level: {
-    type: String,
-    enum: ['Begginer Level', 'Intermediate Level', 'Expert Level', 'All Level'],
-    default: 'Begginer Level'
-  },
-  category : {
-    type: String,
-    enum : ['Development', 'IT & Software','Design', 'Marketing','Teaching * Academics']
-  },
-  price: {
-    type: Number,
-    get: getPrice,
-    set: setPrice
-  },
-  primaryTaught:{
-    type: String,
-  },
-  courseOwner :{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  classes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Class'
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-  rating: {
-    type: Number
+    image: {
+      type: String,
+    },
+    avgRate: {
+      type: Number
+    },
+    students: {
+      type: Number,
+    },
+    price: {
+      type: Number,
+    },
+    currency: {
+      type: String
+    },
+    description: {
+      type: String,
+    },
+    dateUpdated: {
+      type: Date,
+    },
+    totalHours: {
+      type: Number,
+    },
+    Level: {
+      type: String,
+      default: "All levels",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    hasSubtitles: {
+      type: Boolean,
+      default: false,
+    },
+    youWill: {
+      type: [String],
+    },
+    language: {
+      type: String,
+      default: "english",
+    },
+    category: {
+      type: String,
+      enum: [
+        "Development",
+        "IT & Software",
+        "Design",
+        "Marketing",
+        "Teaching & Academics",
+      ],
+    },
+    primaryTaught: [{//
+      id: String,
+      value: String
+    }],
+    courseOwner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    classes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Class",
+      },
+    ],
+    rating: {
+      type: Number,
+    },
+    isPurchased: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isPurchased: {
-    type: Boolean,
-    default: false,
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 module.exports = Course;
--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -13,6 +13,17 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+   /*  validate: {
+      async validator(email) {
+        try {
+          const user = await models.user.findOne({ email })
+          return !user
+        } catch (err) {
+          return false
+        }
+      },
+      message: 'There is an user with this email that already exists'
+    }, */
   },
   password: {
     type: String,
