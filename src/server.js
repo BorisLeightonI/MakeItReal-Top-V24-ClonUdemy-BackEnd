40126e6b7d803b57225852b3fcafef52a1634dfe
BorisLeightonI <borisleighton@gmail.com>
2022-10-20 12:47:32 -0500

    added header allow origin vercer

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(cors({
 //{origin: 'https://mir-top-v24-udemy-front-end.vercel.app'}
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://mir-top-v24-udemy-front-end.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
