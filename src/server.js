d9480a1a00bd1ddede43da68caa7fa522bc8ba0b
Andres Berrio (Andrew) <97372501+andrewsolutions81@users.noreply.github.com>
2022-10-20 11:24:57 -0500

    borrar linea 12 en server (#20)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// require("dotenv").config(); /*Solo se usa en Local no en producción*/
+//require("dotenv").config(); /*Solo se usa en Local no en producción*/
 const morgan = require('morgan');
 const cors = require('cors')
 const { connect } = require("./db")
@@ -19,3 +19,5 @@ applicationRoutes(app)
 app.listen(port, () => {
   console.log(`✅ Server listening on port ${port}!`)
 })
+
+/// hola yo soy un comentario para borrar
