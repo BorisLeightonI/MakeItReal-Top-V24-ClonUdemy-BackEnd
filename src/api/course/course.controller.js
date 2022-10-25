15e43ea60559bfbc0a2b49fbd737c2beb362852d
Andres Berrio (Andrew) <97372501+andrewsolutions81@users.noreply.github.com>
2022-10-25 17:11:56 -0500

    Feat/controller course show put (#34)
    * borrar linea 23 sever 2
    
    * feat/controller-course-show-put

--- src/api/course/course.controller.js
+++ src/api/course/course.controller.js
@@ -1,70 +1,103 @@
-const Course = require('./course.model');
-const User = require('../user/user.model');
+const Course = require("./course.model");
+const User = require("../user/user.model");
 
 const list = (req, res) => {
-  Course.find()/* .populate({
+  Course.find() /* .populate({
     path: 'user',
     select: 'fullName email payment'
   }) */
-  .then( courses => res.status(200).json({message: 'cursos ubicados exitosamente', data: courses}))
-  .catch( err => res.status(200).json({message: 'no se pudo ubicar', data: err}))
-}
+    .then((courses) =>
+      res
+        .status(200)
+        .json({ message: "cursos ubicados exitosamente", data: courses })
+    )
+    .catch((err) =>
+      res.status(200).json({ message: "no se pudo ubicar", data: err })
+    );
+};
+
 //show a course by id
 const show = async (req, res) => {
-try {
-  const { courseId } = req.params;
-  const course = await Course.findById(courseId)/* .populate({
-    path: 'user',
-    select: 'fullName email payment'
-  }) */
-  res.status(200).json({message: 'curso ubicado exitosamente', data: course})
-} catch (error) {
-  res.status(200).json({message: 'no se pudo ubicar el curso', data: error})
-}
+  try {
+    const { courseId } = req.params;
+    const userId = req.user;
+    const course = await Course.findById(courseId);
+    console.log('user id ' , userId,'courseId', courseId)
 
-}
+    if (course.courseOwner.toString() !== userId) {
+      throw new Error("Course not found ,,");
+    }
+
+    res
+      .status(200)
+      .json({ message: "✅ Course found :", data: course });
+  } catch (error) {
+    res
+      .status(200)
+      .json({ message: "no se pudo ubicar el curso", data: error });
+  }
+};
 
 /*create a course*/
 const create = async (req, res) => {
   try {
-    const user = await User.findById(req.user)
+    const user = await User.findById(req.user);
     const data = req.body;
-    if(!user) throw new Error('No existe Usuario');
+    if (!user) throw new Error("No existe Usuario");
     const newCourse = {
       ...data,
-      courseOwner: user._id
-    }
+      courseOwner: user._id,
+    };
     const course = await Course.create(newCourse);
     user.teacherCourses.unshift(course);
-    await user.save({ validateBeforeSave: false })
+    await user.save({ validateBeforeSave: false });
 
-    res.status(201).json({message: 'Curso creado exitosamente', data: course})
+    res
+      .status(201)
+      .json({ message: "Curso creado exitosamente", data: course });
   } catch (err) {
-    res.status(400).json({message: 'No se pudo crear el curso', data: err})
+    res.status(400).json({ message: "No se pudo crear el curso", data: err });
   }
-}
+};
 
-const update = (req, res) => {
-  const { courseId } = req.params;
+const update = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+    const userId = req.user;
+    const currentCourse = await Course.findById(courseId);
 
-  Course.findByIdAndUpdate(courseId, req.body, {new: true})
-    .then( course => res.status(200).json({message: 'curso modificado exitosamente', data: course}))
-    .catch( err => res.status(400).json({message: 'no se pudo modificar', data: err}))
-}
+    if (currentCourse.courseOwner.toString() !== userId) {
+      throw new Error("Course not found");
+    }
+
+    const updatedCourse = await Course.findByIdAndUpdate(courseId, req.body, {new: true});
+
+    res.status(200).json({ message: "✅ Course updated succesfull", data: updatedCourse });
+  } catch (error) {
+    res.status(400).json({ message: "❌ Course could NOT be updated", data: error.message });
+  }
+};
+
+
 
 const destroy = (req, res) => {
   const { courseId } = req.params;
 
   Course.findByIdAndRemove(courseId)
-    .then( course => res.status(200).json({message: 'curso eliminado exitosamente', data: course}))
-    .catch( err => res.status(400).json({message: 'no se pudo eliminar', data: err}))
-}
+    .then((course) =>
+      res
+        .status(200)
+        .json({ message: "curso eliminado exitosamente", data: course })
+    )
+    .catch((err) =>
+      res.status(400).json({ message: "no se pudo eliminar", data: err })
+    );
+};
 
 module.exports = {
-  //course CRUD
   create,
-  show, list,
+  show,
+  list,
   update,
-  destroy
-
-}
+  destroy,
+};
