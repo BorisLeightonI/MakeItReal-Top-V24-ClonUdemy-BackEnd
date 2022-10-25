e2655226013a7a497b9e8ba3fa7470367cd518ce
Maria <zeldacamila0913@gmail.com>
2022-10-24 22:56:18 -0500

    auth without console.log

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,10 +2,8 @@ const jwt = require("jsonwebtoken");
 
 exports.auth = (req, res, next) => {
   try {
-    console.log('req:', req)
     // En al back con minuculas en el front con mayuscula
     const { authorization } = req.headers
-    console.log('auth llegó.', authorization)
     //Para verificar el encabezado llega
     if (!authorization) {
       throw new Error("Su sesión expiró")
