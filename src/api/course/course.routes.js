220c21932e4ffcd260ed41eacafa0136234a35d5
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2022-10-26 11:38:33 -0500

    class creation auth working  commented env in server

--- src/api/course/course.routes.js
+++ src/api/course/course.routes.js
@@ -3,10 +3,10 @@ const courseController = require('./course.controller');
 const { auth } = require('../../utils/auth');
 
 router.route("/").post(auth, courseController.create)
-//router.route("/").post(courseController.createBlankCourse)
-router.route("/").get(courseController.list)
+// router.route("/").post(courseController.createBlankCourse)
+router.route("/").get(auth, courseController.list)
 router.route("/:courseId").get(auth, courseController.show)
 router.route("/:courseId").put(auth, courseController.update)
-router.route("/:courseId").delete(courseController.destroy)
+router.route("/:courseId").delete(auth, courseController.destroy)
 
 module.exports = router;
