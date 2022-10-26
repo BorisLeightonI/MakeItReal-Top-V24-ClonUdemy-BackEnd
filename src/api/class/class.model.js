919c59cbc605decff14e7db7fa9f8102b12bae62
Andres Berrio (Andrew) <97372501+andrewsolutions81@users.noreply.github.com>
2022-10-26 11:25:12 -0500

    Presentation (#38)
    * borrar linea 23 sever 2
    
    * create class and show class working with no auth

--- src/api/class/class.model.js
+++ src/api/class/class.model.js
@@ -1,43 +1,28 @@
 const mongoose = require('mongoose');
 
 const classSchema = new mongoose.Schema({
-  title : {
+  classOfCourse:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true,
+  },
+  classTitle : {
     type: String,
     required: true,
   },
-  description: {
+  classDescription: {
     type: String,
     required: true,
   },
-  category : {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'
-  },
-  price: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'
-  },
-  isActive:{
+  classIsActive:{
     type: Boolean,
     default: true,
   },
-  owner :{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+  classVideo: {
+    type: String,
   },
-  classOfCourse:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'
-  },
-  rating: {
-    type: Number
-  },
-  isPurchased: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'
-  }
 }, { timestamps: true });
 
-const Class = mongoose.model('Class', classSchema);
+const _Class = mongoose.model('Class', classSchema);
 
-module.exports = Class;
+module.exports = _Class;
